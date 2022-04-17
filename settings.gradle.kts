27e581c811d16b2f047ed07b40f64400eef86f26
Andrei Kochemirovskii <blackmorse@live.com>
2022-04-17 19:31:32 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "telegrambot-english"
---
+++ src/main/kotlin/com/blackmorse/telegrambotenglish/TelegrambotEnglishApplication.kt
@@ -0,0 +1,11 @@
+package com.blackmorse.telegrambotenglish
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TelegrambotEnglishApplication
+
+fun main(args: Array<String>) {
+	runApplication<TelegrambotEnglishApplication>(*args)
+}
