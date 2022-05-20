fba88395646b9efd72dc76c0ebff10cb7d314292
Blackmorse <blackmorse@live.com>
2022-05-20 10:04:04 +0300

    Persistence with Cassandra

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,19 @@ dependencies {
 	implementation("com.typesafe.akka:akka-persistence-typed_2.13:2.6.19")
 	implementation("ch.qos.logback:logback-classic:1.2.11")
 	implementation("com.typesafe.akka:akka-serialization-jackson_2.13:2.6.19")
+	implementation("com.typesafe.akka:akka-persistence-cassandra_2.13:1.0.5")
+
+	implementation("com.typesafe.akka:akka-coordination_2.13:2.6.19")
+	implementation("com.typesafe.akka:akka-cluster_2.13:2.6.19")
+	implementation("com.typesafe.akka:akka-cluster-tools_2.13:2.6.19")
+
+	implementation("com.esri.geometry:esri-geometry-api:2.2.4")
+	implementation("org.apache.tinkerpop:gremlin-core:3.6.0")
+	implementation("org.apache.tinkerpop:tinkergraph-gremlin:3.6.0")
+	implementation("org.apache.tinkerpop:gremlin-driver:3.6.0")
+
+
+
 
 	implementation("org.fusesource.leveldbjni:leveldbjni-all:1.8")
 
